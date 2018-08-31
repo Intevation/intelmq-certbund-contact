611365e2086d4d3e46f4e23e9d4066da0573943e
Bernhard Herzog <bernhard.herzog@intevation.de>
2018-08-31 22:37:37 +0200

    Add a simple setup.py file
    This setup.py file contains only a minimal configuration, but it should
    suffice for installation of the code.
    
    The way the entry points for the bots are determined has been copied
    from IntelMQ.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+REQUIRES = [
+    'intelmq>=1.1.0rc2',
+]
+
+BOTS = []
+bots = json.load(open(os.path.join(os.path.dirname(__file__), 'BOTS')))
+for bot_type, bots in bots.items():
+    for bot_name, bot in bots.items():
+        module = bot['module']
+        BOTS.append('{0} = {0}:BOT.run'.format(module))
+
+setup(
+    name='intelmq_certbund_contact',
+    version="0.1",
+    maintainer='Intevation GmbH',
+    maintainer_email='bernhard.herzog@intevation.de',
+    python_requires='>=3.4',
+    install_requires=REQUIRES,
+    packages=find_packages("."),
+    description=('IntelMQ Contacts is a contact database for IntelMQ'
+                 ' with related expert bots'),
+    entry_points={'console_scripts': BOTS},
+)
