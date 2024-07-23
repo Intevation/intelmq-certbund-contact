c042866810c85deeeb3293f45a7578c63df46aa8
Sebastian Wagner <swagner@intevation.de>
2024-07-23 13:26:32 +0200

    update inhibition (whitelist) example rule

--- example-rules/05inhibitions.py
+++ example-rules/05inhibitions.py
@@ -10,8 +10,7 @@ Most of the actual work is done by :py:func:`notification_inhibited`.
 See its documentation for details.
 """
 
-from intelmq_certbund_contact.rulesupport \
-    import notification_inhibited
+from intelmq_certbund_contact.rulesupport import notification_inhibited
 
 """
 This list of Tags defines Tag-names and conditions when a tag matches.
@@ -23,29 +22,42 @@ When using inhibitions, the complex configuration has to be done on the
 contactdb side.
 """
 WHITELIST_TAGS = {
-    "whitelist-malware": {
+    "Whitelist:Malware": {
         "field": "classification.type",
         "values": ["infected-system"]
     },
-    "whitelist-opendns": {
+    "Whitelist:DNS-Open-Resolver": {
         "field": "classification.identifier",
-        "values": ["opendns"]
-    }
+        "values": ["dns-open-resolver"]
+    },
+    "Whitelist:Open-Telnet": {
+        "field": "classification.identifier",
+        "values": ["open-telnet"]
+    },
+    "Whitelist:Shadowserver": {
+        "field": "feed.provider",
+        "values": ["Shadowserver"]
+    },
 }
 
 
 def determine_directives(context):
+    inhibited = False
     if notification_inhibited(context):
         # According to annotations, no notifications should be sent.
         # Assuming this script is run before any notification directives
         # are added to the context, we can simply return True to
         # indicate that no more rules should be processed to inhibit all
         # notifications for this event.
-        return True
-    if inhibited_by_tag(context):
+        inhibited = True
+    if not inhibited and inhibited_by_tag(context):
         # According to the Tags of the Organisation or Network
         # This event should not be reported.
-        return True
+        inhibited = True
+
+    remove_inhibition_matches(context)
+
+    return inhibited
 
 
 def inhibited_by_tag(context):
@@ -60,10 +72,29 @@ def inhibited_by_tag(context):
         if a.tag != "inhibition":
             tags.add(a.tag)
 
+    if "Whitelist:All" in tags:
+        return True
+
     for t in tags.intersection(WHITELIST_TAGS):
         field = WHITELIST_TAGS[t]["field"]
         values = WHITELIST_TAGS[t]["values"]
         if context.get(field) in values:
             return True
 
+    return False
+
+def remove_inhibition_matches(context):
+    context.matches = [match for match in context.matches
+                       if match.field != "ip" or not has_inhibitions(match)]
+
+    org_ids = set(i for match in context.matches for i in match.organisations)
+    context.organisations = [org for org in context.organisations
+                             if org.orgid in org_ids]
+
+
+def has_inhibitions(match):
+    inhibition_tags = set(["inhibition", "Whitelist:All"]) | WHITELIST_TAGS.keys()
+    for annotation in match.annotations:
+        if annotation.tag in inhibition_tags:
+            return True
     return False
