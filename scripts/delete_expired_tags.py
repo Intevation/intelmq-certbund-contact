e8d103ee511e9794d8d37f1adc461eee348fb6e0
Sebastian Wagner <swagner@intevation.de>
2025-07-08 16:06:26 +0200

    delete expired tags: support for absolute dates
    support both relative and absolute dates, detect them
    add documentation and examples

--- scripts/delete_expired_tags.py
+++ scripts/delete_expired_tags.py
@@ -9,13 +9,15 @@ pytest scripts/expire_all_tags.py
 """
 import argparse
 import psycopg2
+from datetime import datetime
+from textwrap import dedent
 from psycopg2.extras import Json
 from psycopg2.extensions import register_adapter
 register_adapter(dict, Json)
 
 SQL_GET_EXPIRED_ANNOTATIONS = """
     SELECT {table}_annotation_id, {index}, annotation FROM {table}_annotation
-    WHERE (annotation ->> 'expires')::date < NOW() - INTERVAL %s AND annotation ->> 'expires' IS NOT NULL AND annotation ->> 'expires' != ''""".replace('\n    ', ' ')
+    WHERE (annotation ->> 'expires')::date < {timespec} %s AND annotation ->> 'expires' IS NOT NULL AND annotation ->> 'expires' != ''""".replace('\n    ', ' ')
 SQL_DELETE_ANNOTATION = """
     DELETE FROM {table}_annotation
     WHERE {table}_annotation_id = %s""".replace('\n    ', ' ')
@@ -49,22 +51,38 @@ def _get_name_to_object(object_type: str, object_value: int, cur) -> str:
 
 
 def main():
-    parser = argparse.ArgumentParser('delete_expired_tags')
+    parser = argparse.ArgumentParser('delete_expired_tags',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=dedent('''
+        Setting the age to 2025-07-07 deletes all tags that are expired on 2025-07-06 or earlier, but not the tags that expire on 2025-07-07 itself.
+        Setting the age to '0 days' on 2025-07-08 deletes all tags that are expired, on 2025-07-07 or earlier.
+        Setting the age to '1 day' on 2025-07-08 deletes all tags that are expired before 2025-07-07: on 2025-07-06 or earlier.
+        '''))
     parser.add_argument('-v', '--verbose', action='count', default=0)
     parser.add_argument('-n', '--dry-run', action='store_true')
-    parser.add_argument('age', help="Postgres-parseable date interval, for example '1 month'")
+    parser.add_argument('age', help="Either a Postgres-parseable date interval, for example '1 month' or an absolute date, formatted %%Y-%%m-%%d '2020-01-01'.")
     parser.add_argument('-u', '--audit-log-user', default=DEFAULT_AUDIT_LOG_USER,
                         help=f'The username to appear in the audit log for deleting the expired annotations. Default: {DEFAULT_AUDIT_LOG_USER}')
     args = parser.parse_args()
+
+    try:  # Test if the age is an absolute date
+        datetime.strptime(args.age, '%Y-%m-%d')
+    except ValueError:  # no it's not absolute -> relative
+        time_is_relative = True
+    else:
+        time_is_relative = False
+    if args.verbose:
+        print(f'Given time specifier is relative: {time_is_relative}')
 
     from contactdb_api.contactdb_api.serve import read_configuration
     with psycopg2.connect(dsn=read_configuration()["libpg conninfo"]) as conn:
         with conn.cursor() as cur:
             for table in ('organisation', 'autonomous_system', 'network', 'fqdn'):
                 print(f'Processing {table}_annotation')
+                select_query = SQL_GET_EXPIRED_ANNOTATIONS.format(table=table, index=TABLE_TO_INDEX[table], timespec='CURRENT_DATE - INTERVAL' if time_is_relative else '')
                 if args.verbose >= 2:
-                    print(cur.mogrify(SQL_GET_EXPIRED_ANNOTATIONS.format(table=table, index=TABLE_TO_INDEX[table]), (args.age, )).decode())
-                cur.execute(SQL_GET_EXPIRED_ANNOTATIONS.format(table=table, index=TABLE_TO_INDEX[table]), (args.age, ))
+                    print(cur.mogrify(select_query, (args.age, )).decode())
+                cur.execute(select_query, (args.age, ))
                 for expired_anno in cur.fetchall():
                     anno_id, foreign_id, annotation = expired_anno
                     if args.verbose:
