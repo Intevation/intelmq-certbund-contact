70303a20bcac3f07d75ffa93a6a7d450b64cb36b
Paul Höller <199104660+paul-hoeller@users.noreply.github.com>
2025-09-12 15:35:29 +0200

    Add test case
    Add test case for case normalization of email addresses.

--- tests/ripe/test_ripe_data.py
+++ tests/ripe/test_ripe_data.py
@@ -169,6 +169,24 @@ class TestRipeData(unittest.TestCase):
             ],
         )
 
+    def test_case_normalization(self):
+        role_list = ripe_data.parse_file(
+            f"{dirname(__file__)}/role_simple_uppercase.txt.gz",
+            ("role", "nic-hdl", "abuse-mailbox", "org"),
+            verbose=True,
+        )
+        sanitized = [dict(element) for element in ripe_data.sanitize_role_list(role_list)]
+        self.assertEqual(
+            sanitized,
+            [
+                {
+                    "abuse-mailbox": ["null@example.com"],
+                    "nic-hdl": ["ACRO6281-RIPE"],
+                    "role": ["Null"],
+                }
+            ],
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
