a58839dca2e9534fe114c4c0e6d57cb1292c0a2e
Paul Höller <199104660+paul-hoeller@users.noreply.github.com>
2025-09-26 17:02:54 +0200

    Update emails_to_lowercase.py
    Use the lower() function
    
    (Hopefully there will be no Unicode issues)

--- scripts/emails_to_lowercase.py
+++ scripts/emails_to_lowercase.py
@@ -6,23 +6,14 @@ In the tables contact_automatic and contact, convert all values in the email col
 """
 import psycopg2
 
-_CURSOR_NAME = 'email_lowercase_cursor'
-
 def main():
     from contactdb_api.contactdb_api.serve import read_configuration
     conn = psycopg2.connect(dsn=read_configuration()["libpg conninfo"])
     try:
         with conn:
-            # https://stackoverflow.com/a/67729179
-            with conn.cursor() as update_cur:
-                for table in ('contact_automatic', 'contact'):
-                    with conn.cursor(name=_CURSOR_NAME) as select_cur:
-                        select_cur.itersize = 1
-                        select_cur.execute(f'SELECT email FROM {table} FOR UPDATE')
-                        for email, in select_cur:
-                            email_lower = email.encode().lower().decode()
-                            if email != email_lower:
-                                update_cur.execute(f'UPDATE {table} SET email = %s WHERE CURRENT OF {_CURSOR_NAME}', (email_lower,))
+            for table in ('contact_automatic', 'contact'):
+                with conn.cursor() as cur:
+                    cur.execute(f"UPDATE {table} SET email = lower(email) WHERE email ~ '[A-Z]'")
     finally:
         # Have to manually close connection even when using with-statement:
         # https://www.psycopg.org/docs/usage.html#with-statement
